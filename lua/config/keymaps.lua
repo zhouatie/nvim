8f8c08e087caa789686835dfe320308ce5055bda
zhoushitie <zhoushitie@corp.netease.com>
2023-10-06 15:29:23 +0800

    feat: 添加一些插件

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,27 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local keymap = vim.keymap
+--
+-- local keymap = vim.keymap
+--
+-- -- jk 退出编辑模式
+-- keymap.set("i", "jk", "<Esc>")
+-- keymap.set("in", "jkl", "<cmd>w<cr><esc>")
 
--- jk 退出编辑模式
-keymap.set("i", "jk", "<Esc>")
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+map("i", "jk", "<Esc>", { desc = "to normal mode" })
+
+-- map({ "n", "i" }, "jkl", "<cmd>w<cr><esc>", { desc = "save" })
---
+++ todo.md
@@ -0,0 +1,9 @@
+## todo
+
+### 不知道是什么用
+
+<leader>sm Jump to Mark
+<leader>so Options
+<leader>sR Resume
+<leader>ss Goto Symbol
+lazyvim dap 调试
