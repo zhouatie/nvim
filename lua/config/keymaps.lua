f42501e5d4722cb9d5ec96908e449edb0f5ad135
zhoushitie <zhoushitie@corp.netease.com>
2023-10-06 12:11:03 +0800

    feat: 添加jk快捷键，添加ts lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap
+
+-- jk 退出编辑模式
+keymap.set("i", "jk", "<Esc>")
