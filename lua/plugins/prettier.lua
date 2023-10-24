5eae1856af37d5c1bcd53da44025b0e2bf094a49
zhoushitie <zhoushitie@corp.netease.com>
2023-10-24 19:00:44 +0800

    fix: 用stylelint 去格式化样式文件

--- lua/plugins/prettier.lua
+++ lua/plugins/prettier.lua
@@ -1,43 +1,60 @@
 -- 重置prettier
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      print(opts)
-      table.insert(opts.ensure_installed, "prettierd")
-    end,
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      print(opts)
-      local nls = require("null-ls")
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, nls.builtins.formatting.prettierd)
-    end,
-  },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     print(opts)
+  --     table.insert(opts.ensure_installed, "prettierd")
+  --   end,
+  -- },
+  -- {
+  --   "nvimtools/none-ls.nvim",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     print(opts)
+  --     local nls = require("null-ls")
+  --     opts.sources = opts.sources or {}
+  --     table.insert(opts.sources, nls.builtins.formatting.prettierd)
+  --   end,
+  -- },
   {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
+
       formatters_by_ft = {
-        -- ["javascript"] = { { "prettierd", "prettier" } },
-        -- ["javascriptreact"] = { { "prettierd", "prettier" } },
-        -- ["typescript"] = { { "prettierd", "prettier" } },
-        -- ["typescriptreact"] = { { "prettierd", "prettier" } },
-        -- ["vue"] = { { "prettierd", "prettier" } },
-        ["css"] = { { "prettierd", "prettier" } },
-        ["scss"] = { { "prettierd", "prettier" } },
-        ["less"] = { { "prettierd", "prettier" } },
-        ["html"] = { { "prettierd", "prettier" } },
-        ["json"] = { { "prettierd", "prettier" } },
-        ["jsonc"] = { { "prettierd", "prettier" } },
-        ["yaml"] = { { "prettierd", "prettier" } },
-        ["markdown"] = { { "prettierd", "prettier" } },
-        ["markdown.mdx"] = { { "prettierd", "prettier" } },
-        ["graphql"] = { { "prettierd", "prettier" } },
-        ["handlebars"] = { { "prettierd", "prettier" } },
+        -- ["javascript"] = { "prettier" },
+        -- ["javascriptreact"] = { "prettier" },
+        -- ["typescript"] = { "prettier" },
+        -- ["typescriptreact"] = { "prettier" },
+        ["vue"] = { "prettier" },
+        ["css"] = { "stylelint" },
+        ["scss"] = { "stylelint" },
+        ["less"] = { "stylelint" },
+        ["html"] = { "prettier" },
+        ["json"] = { "prettier" },
+        ["jsonc"] = { "prettier" },
+        ["yaml"] = { "prettier" },
+        ["markdown"] = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+        ["graphql"] = { "prettier" },
+        ["handlebars"] = { "prettier" },
+        --   -- ["javascript"] = { { "prettierd", "prettier" } },
+        --   -- ["javascriptreact"] = { { "prettierd", "prettier" } },
+        --   -- ["typescript"] = { { "prettierd", "prettier" } },
+        --   -- ["typescriptreact"] = { { "prettierd", "prettier" } },
+        --   -- ["vue"] = { { "prettierd", "prettier" } },
+        --   ["css"] = { { "prettierd", "prettier" } },
+        --   ["scss"] = { { "prettierd", "prettier" } },
+        --   ["less"] = { { "prettierd", "prettier" } },
+        --   ["html"] = { { "prettierd", "prettier" } },
+        --   ["json"] = { { "prettierd", "prettier" } },
+        --   ["jsonc"] = { { "prettierd", "prettier" } },
+        --   ["yaml"] = { { "prettierd", "prettier" } },
+        --   ["markdown"] = { { "prettierd", "prettier" } },
+        --   ["markdown.mdx"] = { { "prettierd", "prettier" } },
+        --   ["graphql"] = { { "prettierd", "prettier" } },
+        --   ["handlebars"] = { { "prettierd", "prettier" } },
       },
     },
   },
