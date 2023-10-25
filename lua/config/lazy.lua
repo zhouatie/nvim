ef283e26c7547533bd41f467fe8ccbcc6d365101
zhoushitie <zhoushitie@corp.netease.com>
2023-10-25 12:04:55 +0800

    feat: 添加复制插件，不知道有啥功能，加了再说

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
     -- { import = "lazyvim.plugins.extras.ui.alpha" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.formatting.conform" },
