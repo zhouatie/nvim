5e40cc1c9fc34240bb94829239fd56c24301a5fc
zhoushitie <zhoushitie@corp.netease.com>
2023-10-27 11:21:09 +0800

    feat: 升级插件

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
+    -- { import = "lazyvim.plugins.extras.lang.tailwind" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.formatting.conform" },
     -- import/override with your plugins
