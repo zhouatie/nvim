64ed96713f199cee519dccc729cba0e6570e46df
zhouatie <zhouatie@gmail.com>
2024-07-19 19:59:32 +0800

    fix: 去掉 inlay_hints

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    cmd = "Neotree",
+    opts = {
+      window = {
+        mappings = {
+          ["o"] = "open",
+        },
+      },
+    },
+  },
+}
