092d0e36620b69a36f8d743a1191160454fa5d6b
zhouatie <zhouatie@gmail.com>
2025-04-11 18:31:54 +0800

    fix: 去掉 neogit

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -104,16 +104,16 @@ return {
   },
 
   -- neogit
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- required
-      "sindrets/diffview.nvim", -- optional - Diff integration
-
-      -- Only one of these is needed.
-      "nvim-telescope/telescope.nvim", -- optional
-      "ibhagwan/fzf-lua", -- optional
-      "echasnovski/mini.pick", -- optional
-    },
-  },
+  -- {
+  --   "NeogitOrg/neogit",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim", -- required
+  --     "sindrets/diffview.nvim", -- optional - Diff integration
+  --
+  --     -- Only one of these is needed.
+  --     "nvim-telescope/telescope.nvim", -- optional
+  --     "ibhagwan/fzf-lua", -- optional
+  --     "echasnovski/mini.pick", -- optional
+  --   },
+  -- },
 }
