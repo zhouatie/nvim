33cfec40bf31c5ba0e09ad70624d33566b6afa74
zhouatie <zhouatie@gmail.com>
2025-04-19 17:05:19 +0800

    feat: 添加酷炫光标

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -1,4 +1,16 @@
 return {
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      stiffness = 0.8,
+      trailing_stiffness = 0.5,
+      stiffness_insert_mode = 0.6,
+      trailing_stiffness_insert_mode = 0.6,
+      distance_stop_animating = 0.5,
+      cursor_color = "#47FF9C",
+      legacy_computing_symbols_support = true,
+    },
+  },
   --- git-blame
   {
     "f-person/git-blame.nvim",
