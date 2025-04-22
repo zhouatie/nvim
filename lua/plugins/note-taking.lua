5df6e05edfce178bbc861005228086f94302e2f9
zhouatie <zhouatie@gmail.com>
2025-04-22 20:53:22 +0800

    fix: 修改 dooing 弹窗ui 配置

--- lua/plugins/note-taking.lua
+++ lua/plugins/note-taking.lua
@@ -68,6 +68,17 @@ return {
         return suffix
       end,
 
+      -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+      -- URL it will be ignored but you can customize this behavior here.
+      ---@param url string
+      follow_url_func = function(url)
+        -- Open the URL in the default web browser.
+        vim.fn.jobstart({ "open", url }) -- Mac OS
+        -- vim.fn.jobstart({"xdg-open", url})  -- linux
+        -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+        -- vim.ui.open(url) -- need Neovim 0.10.0+
+      end,
+
       -- Optional, customize how wiki links are formatted. You can set this to one of:
       --  * "use_alias_only", e.g. '[[Foo Bar]]'
       --  * "prepend_note_id", e.g. '[[foo-bar|Foo Bar]]'
@@ -176,8 +187,8 @@ return {
 
         -- Window settings
         window = {
-          width = 85, -- Width of the floating window
-          height = 20, -- Height of the floating window
+          width = 100, -- Width of the floating window
+          height = 15, -- Height of the floating window
           border = "rounded", -- Border style
           position = "center", -- Window position: 'right', 'left', 'top', 'bottom', 'center',
           -- 'top-right', 'top-left', 'bottom-right', 'bottom-left'
