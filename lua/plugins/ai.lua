7cbd48329c1ee819d2773805a8dd7c9ba1b3e789
zhouatie <zhouatie@gmail.com>
2025-05-04 20:30:52 +0800

    feat: avante支持blink，支持url-open插件

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,9 +1,16 @@
+local function tokens(num)
+  return num * 1024
+end
+
 return {
+  { "augmentcode/augment.vim" },
+
   {
+    -- dir = "~/.config/nvim/clone/avante.nvim",
     "yetone/avante.nvim",
     event = "VeryLazy",
-    -- version = false,
-    commit = "f9aa75459d403d9e963ef2647c9791e0dfc9e5f9",
+    version = false,
+    -- commit = "f9aa75459d403d9e963ef2647c9791e0dfc9e5f9",
     -- version = "*", -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
     opts = {
       provider = "copilot",
@@ -11,14 +18,16 @@ return {
       copilot = {
         model = "claude-3.7-sonnet",
         -- model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-        -- endpoint = "https://api.githubcopilot.com",
+        -- model = "o3-mini",
+        endpoint = "https://api.githubcopilot.com",
+        proxy = nil,
         allow_insecure = false,
         timeout = 10 * 60 * 1000,
         temperature = 0,
         -- 8192
-        max_completion_tokens = 80000,
-        max_tokens = 80000,
-        reasoning_effort = "high",
+        max_completion_tokens = 20480,
+        max_tokens = 20480,
+        -- reasoning_effort = "high",
       },
 
       behaviour = {
@@ -35,23 +44,56 @@ return {
         },
       },
 
-      -- ollama = {
-      --   model = "qwen2.5-coder:14b",
-      -- },
+      ollama = {
+        model = "qwen2.5-coder:14b",
+      },
 
       -- 可以用
-      -- claude = {
-      --   endpoint = "https://aihubmix.com/v1",
-      --   model = "claude-3-7-sonnet-20250219",
-      --   -- model = "Qwen/QwQ-32B",
-      --   -- stream = true,
-      --   -- model = "llama-3.3-70b-versatile",
-      --   -- model = "claude-3-5-sonnet-20240620",
-      --   -- model = "aihubmix-DeepSeek-R1",
-      --   timeout = 30000, -- timeout in milliseconds
-      --   temperature = 0, -- adjust if needed
-      --   max_tokens = 8192,
-      -- },
+      aihubmix = {
+        endpoint = "https://aihubmix.com/v1",
+        model = "claude-3-7-sonnet-20250219",
+        -- model = "Qwen/QwQ-32B",
+        -- stream = true,
+        -- model = "llama-3.3-70b-versatile",
+        -- model = "claude-3-5-sonnet-20240620",
+        -- model = "aihubmix-DeepSeek-R1",
+        timeout = 30000, -- timeout in milliseconds
+        temperature = 0, -- adjust if needed
+        max_tokens = 8192,
+      },
+      vendors = {
+        ["copilot:3.7"] = {
+          __inherited_from = "copilot",
+          model = "claude-3.7-sonnet",
+          max_tokens = tokens(64),
+        },
+        ["copilot:o3"] = {
+          __inherited_from = "copilot",
+          model = "o3-mini",
+          max_tokens = tokens(64),
+        },
+        deepseek = {
+          __inherited_from = "openai",
+          api_key_name = "DEEPSEEK_API_KEY",
+          endpoint = "https://api.deepseek.com",
+          model = "deepseek-coder",
+        },
+        qianwen = {
+          __inherited_from = "openai",
+          api_key_name = "DASHSCOPE_API_KEY",
+          endpoint = "https://dashscope.aliyuncs.com/compatible-mode/v1",
+          model = "qwen-coder-plus-latest",
+        },
+      },
+
+      rag_service = {
+        enabled = true, -- Enables the RAG service
+        host_mount = os.getenv("HOME"), -- Host mount path for the rag service
+        provider = "ollama", -- The provider to use for RAG service (e.g. openai or ollama)
+        llm_model = "qwen2.5-coder:14b", -- The LLM model to use for RAG service
+        embed_model = "nomic-embed-text", -- The embedding model to use for RAG service
+        endpoint = "http://localhost:11434", -- The API endpoint for RAG service
+      },
     },
     build = "make",
     dependencies = {
@@ -73,5 +115,64 @@ return {
         ft = { "markdown", "Avante" },
       },
     },
+  },
+
+  {
+    "saghen/blink.cmp",
+    --optional = true,
+    opts = {
+      completion = {
+        menu = {
+          draw = {
+            columns = {
+              { "label", "label_description", gap = 1 },
+              { "kind_icon", "source_name" },
+            },
+            treesitter = { "lsp" },
+          },
+        },
+      },
+
+      sources = {
+        default = { "avante_commands", "avante_mentions", "avante_files" },
+        compat = {
+          "avante_commands",
+          "avante_mentions",
+          "avante_files",
+        },
+        -- LSP score_offset is typically 60
+        providers = {
+          avante_commands = {
+            name = "avante_commands",
+            module = "blink.compat.source",
+            score_offset = 90,
+            opts = {},
+          },
+          avante_files = {
+            name = "avante_files",
+            module = "blink.compat.source",
+            score_offset = 100,
+            opts = {},
+          },
+          avante_mentions = {
+            name = "avante_mentions",
+            module = "blink.compat.source",
+            score_offset = 1000,
+            opts = {},
+          },
+        },
+      },
+
+      keymap = {
+        preset = "enter",
+        ["<C-y>"] = { "select_and_accept" },
+        ["<C-o>"] = { "show" },
+        ["<Tab>"] = {
+          LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+          "fallback",
+        },
+      },
+    },
+    dependencies = { "saghen/blink.compat" },
   },
 }
