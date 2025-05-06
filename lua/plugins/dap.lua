a7dd7044d9f56b33b6ed46f1eec99323b8147d42
zhouatie <zhouatie@gmail.com>
2025-05-06 19:53:15 +0800

    feat: debug

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -164,13 +164,13 @@ return {
     end,
   },
 
-  -- {
-  --   "miroshQa/debugmaster.nvim",
-  --   config = function()
-  --     local dm = require("debugmaster")
-  --     -- make sure you don't have any other keymaps that starts with "<leader>d" to avoid delay
-  --     vim.keymap.set({ "n", "v" }, "<leader>d", dm.mode.toggle, { nowait = true })
-  --     vim.keymap.set("t", "<C-/>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-  --   end,
-  -- },
+  {
+    "miroshQa/debugmaster.nvim",
+    config = function()
+      local dm = require("debugmaster")
+      -- make sure you don't have any other keymaps that starts with "<leader>d" to avoid delay
+      vim.keymap.set({ "n", "v" }, "<leader>d", dm.mode.toggle, { nowait = true })
+      vim.keymap.set("t", "<C-/>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+    end,
+  },
 }
