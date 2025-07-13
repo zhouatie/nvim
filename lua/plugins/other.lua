286e47db0dfc34ec467b0615f41423f35f4d0d81
zhouatie <zhouatie@gmail.com>
2025-07-14 00:44:25 +0800

    feat: 支持 neogit

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -1,5 +1,30 @@
 return {
   {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua", -- optional
+      "echasnovski/mini.pick", -- optional
+      "folke/snacks.nvim", -- optional
+    },
+    keys = {
+      {
+        "<leader>gg",
+        function()
+          require("neogit").open()
+        end,
+        desc = "Open Neogit",
+      },
+    },
+    config = function()
+      require("neogit").setup()
+    end,
+  },
+  {
     "sontungexpt/url-open",
     event = "VeryLazy",
     cmd = "URLOpenUnderCursor",
