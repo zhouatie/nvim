530d50c3f3417654ce1d50bc531eda688212f03e
zhouatie <zhouatie@gmail.com>
2025-07-21 10:04:01 +0800

    feat: 2025-07-21

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -1,29 +1,32 @@
 return {
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- required
-      "sindrets/diffview.nvim", -- optional - Diff integration
 
-      -- Only one of these is needed.
-      "nvim-telescope/telescope.nvim", -- optional
-      "ibhagwan/fzf-lua", -- optional
-      "echasnovski/mini.pick", -- optional
-      "folke/snacks.nvim", -- optional
-    },
-    keys = {
-      {
-        "<leader>gg",
-        function()
-          require("neogit").open()
-        end,
-        desc = "Open Neogit",
-      },
-    },
-    config = function()
-      require("neogit").setup()
-    end,
-  },
+  -- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
+
+  -- {
+  --   "NeogitOrg/neogit",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim", -- required
+  --     "sindrets/diffview.nvim", -- optional - Diff integration
+  --
+  --     -- Only one of these is needed.
+  --     "nvim-telescope/telescope.nvim", -- optional
+  --     "ibhagwan/fzf-lua", -- optional
+  --     "echasnovski/mini.pick", -- optional
+  --     "folke/snacks.nvim", -- optional
+  --   },
+  --   keys = {
+  --     {
+  --       "<leader>gg",
+  --       function()
+  --         require("neogit").open()
+  --       end,
+  --       desc = "Open Neogit",
+  --     },
+  --   },
+  --   config = function()
+  --     require("neogit").setup()
+  --   end,
+  -- },
   {
     "sontungexpt/url-open",
     event = "VeryLazy",
