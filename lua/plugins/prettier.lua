12918bc5f547f383934a12a749dcb2619fcb0eb1
zhouatie <zhouatie@gmail.com>
2025-12-02 15:49:44 +0800

    feat: update

--- lua/plugins/prettier.lua
+++ lua/plugins/prettier.lua
@@ -1,5 +1,29 @@
 return {
   {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    opts = {
+      options = {
+        multilines = {
+          enabled = true,
+        },
+        show_source = {
+          enabled = true,
+        },
+        add_messages = {
+          display_count = true,
+        },
+        set_arrow_to_diag_color = true,
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = { diagnostics = { virtual_text = false } },
+  },
+
+  {
     "stevearc/conform.nvim",
     lazy = true,
     optional = true,
